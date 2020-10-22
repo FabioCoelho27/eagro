cfc7e86ef963cd2b69f39cfb5ca0a034c44b3ab2
FábioCoelho27 <fabioolivc@gmail.com>
2020-10-22 12:22:37 -0300

    Starting project and creating model and controller

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Task.create(title: 'Reunião (01/11/2020) as (15:00 horas)', done: false)
+Task.create(title: 'Curso de React dia (05/11/2020)', done: false)
+Task.create(title: 'Automatizar testes', done: false)
+Task.create(title: 'Criar um model para usuarios', done: false)
+Task.create(title: 'Finalizar Projeto', done: false)
